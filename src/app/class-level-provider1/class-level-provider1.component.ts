631a12d43835aad2593286d83c7b6214d987ea01
Krishna Chegonda <krishna.chegonda@pearson.com>
2023-09-22 19:15:22 +0530

    implement interfaces with its methods to follow angular fraework design patterns, we can develop app witthout them too but does not make any sense

--- src/app/class-level-provider1/class-level-provider1.component.ts
+++ src/app/class-level-provider1/class-level-provider1.component.ts
@@ -1,15 +1,23 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-provider',
   templateUrl: './class-level-provider1.component.html',
   styleUrls: ['./class-level-provider1.component.css']
 })
-export class ClassLevelProvider1Component implements OnInit {
+export class ClassLevelProvider1Component  implements OnInit, OnDestroy {
+
 
   constructor() { }
 
-  ngOnInit(): void {
+  ngOnInit() {
+    // This code will be executed when the component is initialized
+    console.log('Component initialized');
+  }
+
+  ngOnDestroy() {
+    // This code will be executed when the component is about to be destroyed
+    console.log('Component destroyed');
   }
 
 }
